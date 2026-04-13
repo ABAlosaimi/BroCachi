e179e1ae076637a0b2ec4c38b3cb12a43774fa18
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-04-13 04:03:08 +0300

    update: the problem with protoc solved by using the protoc command instead of makefile

--- services/orders/handler/grpc/grpcHnadler.go
+++ services/orders/handler/grpc/grpcHnadler.go
@@ -1,9 +1,19 @@
 package grpc
 
 import (
+	"github.com/ABAlosaimi/BroCachi/services/common/protoGen"
 	"github.com/ABAlosaimi/BroCachi/services/orders/types"
 )
 
 type OrderGRPCHandler struct {
 	orderService types.OrderService
+	protoGen.UnimplementedOrderServiceServer
 }
+
+func NewOrderGRPCHandler(orderService types.OrderService) *OrderGRPCHandler {
+	return &OrderGRPCHandler{
+		orderService: orderService,
+	}
+}
+
+// implement the gRPC methods here
